8f4fcb07e3a245c7c2459ed59fccece491975836
LawrenceHollom <lawrence@hollom.com>
2026-06-30 09:06:26 +0100

    Look at more stuff

--- rss_scraper.py
+++ rss_scraper.py
@@ -1,5 +1,18 @@
+import xmltodict
+import pprint
 import urllib.request
 
-url = "https://rss.arxiv.org/rss/math.CO"
+url = "https://rss.arxiv.org/rss/math.CO+math.NT+math.PR+math.LO"
 with urllib.request.urlopen(url) as response:
-    print(response.read().decode("utf-8"))
+    text = response.read().decode("utf-8")
+    papers_list = xmltodict.parse(text)['rss']['channel']['item']
+
+    out_list = []
+
+    for paper in papers_list:
+        # pprint.pprint(paper, indent=2)
+        if paper['arxiv:announce_type'] == 'new':
+            out_list.append(paper)
+
+    # Print the dictionary
+    print(out_list)
